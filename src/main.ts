f9a6cb0619d0fe4ee86268399f39d3bece660bc9
Yuan <nguyenshort@gmail.com>
2022-07-27 09:00:07 +0700

    refactor categories

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import {MicroserviceOptions, Transport} from "@nestjs/microservices";
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {MicroserviceOptions} from "@nestjs/microservices";
 import {Logger} from "@nestjs/common";
+import {buildRMQ, ComicoQueue} from "@comico/shared";
 
 async function bootstrap() {
   const logger = new Logger('Microservice')
 
   const app = await NestFactory.create(AppModule)
 
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: ['amqp://localhost:5672'],
-      queue: 'stories_queue',
-      queueOptions: {
-        durable: false
-      }
-    }
-  })
+  app.connectMicroservice<MicroserviceOptions>(buildRMQ({
+    urls: ['amqp://localhost:5672'],
+    queue: ComicoQueue.STORIES
+  }))
 
 
   await app.startAllMicroservices()
